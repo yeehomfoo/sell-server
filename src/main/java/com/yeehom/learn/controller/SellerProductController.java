ee4b6e1be0214914fe81001933f5df43cf702bfa
Yeehom Foo <yeehom147@gmail.com>
2018-11-17 01:59:46 +0800

    add product list

---
+++ src/main/java/com/yeehom/learn/controller/SellerProductController.java
@@ -0,0 +1,45 @@
+package com.yeehom.learn.controller;
+
+import com.yeehom.learn.Util.ResultVOUtil;
+import com.yeehom.learn.VO.ResultVO;
+import com.yeehom.learn.dataobject.ProductInfo;
+import com.yeehom.learn.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("/seller/product")
+public class SellerProductController {
+
+    @Autowired
+    ProductService productService;
+
+    /**
+     * 商品列表
+     * @param page
+     * @param size
+     * @param map
+     * @return
+     */
+    @GetMapping("/list")
+    public ModelAndView list(@RequestParam(value = "page", defaultValue = "1") Integer page,
+                             @RequestParam(value = "size", defaultValue = "20") Integer size,
+                             Map<String, Object> map) {
+
+        PageRequest pageRequest = new PageRequest(page - 1, size);
+        Page<ProductInfo> productInfoPage = productService.findAll(pageRequest);
+        map.put("productInfoPage", productInfoPage);
+        map.put("currentPage", page);
+        map.put("size", size);
+        return new ModelAndView("/product/list", map);
+    }
+}
--- src/main/java/com/yeehom/learn/dataobject/ProductInfo.java
+++ src/main/java/com/yeehom/learn/dataobject/ProductInfo.java
@@ -1,16 +1,22 @@
 package com.yeehom.learn.dataobject;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.yeehom.learn.Util.EnumUtil;
+import com.yeehom.learn.enums.ProductStatusEnum;
 import lombok.Data;
+import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.math.BigDecimal;
+import java.util.Date;
 
 /**
  * Created by yFoo on 13/01/2018.
  */
 @Entity
 @Data
+@DynamicUpdate
 public class ProductInfo {
 
     @Id
@@ -29,4 +35,14 @@ public class ProductInfo {
     private Integer productStatus;
 
     private Integer categoryType;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+
+    @JsonIgnore
+    public ProductStatusEnum getProductStatusEnum() {
+        return EnumUtil.getByCode(this.productStatus, ProductStatusEnum.class);
+    }
 }
