155092a0b8b391eb80feece875388555cd492d31
Yeehom Foo <yeehom147@gmail.com>
2018-11-21 01:21:01 +0800

    refactor: try to use Mybatis instead of JPA

--- src/main/java/com/yeehom/learn/SellApplication.java
+++ src/main/java/com/yeehom/learn/SellApplication.java
@@ -1,9 +1,11 @@
 package com.yeehom.learn;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.yeehom.learn.dataobject.mapper")
 public class SellApplication {
 
 	public static void main(String[] args) {
---
+++ src/test/java/com/yeehom/learn/dataobject/mapper/ProductCategoryMapperTest.java
@@ -0,0 +1,96 @@
+package com.yeehom.learn.dataobject.mapper;
+
+import com.yeehom.learn.dataobject.ProductCategory;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author yFoo
+ * @date ${date}
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductCategoryMapperTest {
+
+    @Autowired
+    private ProductCategoryMapper mapper;
+
+    @Test
+    public void insertByMap() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("categoryName", "我的最爱");
+        map.put("category_type", 102);
+        int count = mapper.insertByMap(map);
+
+        Assert.assertEquals(1, count);
+    }
+
+    @Test
+    public void insertByObject() {
+        ProductCategory productCategory = new ProductCategory();
+        productCategory.setCategoryName("我的最爱");
+        productCategory.setCategoryType(103);
+        int count = mapper.insertByObject(productCategory);
+
+        Assert.assertEquals(1, count);
+    }
+
+    @Test
+    public void findByCategoryType() {
+        ProductCategory result = mapper.findByCategoryType(103);
+
+        Assert.assertNotNull(result);
+    }
+
+    @Test
+    public void findByCategoryName() {
+        List<ProductCategory> result = mapper.findByCategoryName("我的最爱");
+
+        Assert.assertNotEquals(0, result.size());
+    }
+
+    @Test
+    public void updateByMap() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("categoryType", 103);
+        map.put("categoryName", "我的最不爱");
+        int count = mapper.updateByMap(map);
+
+        Assert.assertEquals(1, count);
+    }
+
+    @Test
+    public void updateByCategoryType() {
+        int count = mapper.updateByCategoryType(103, "我的最爱");
+
+        Assert.assertEquals(1, count);
+    }
+
+    @Test
+    public void updateByObject() {
+        ProductCategory productCategory = new ProductCategory();
+        productCategory.setCategoryType(103);
+        productCategory.setCategoryName("我的最不爱");
+        int count = mapper.updateByObject(productCategory);
+
+        Assert.assertEquals(1, count);
+    }
+
+    @Test
+    public void deleteByCategoryType() {
+        int count = mapper.deleteByCategoryType(103);
+
+        Assert.assertEquals(1, count);
+    }
+}
