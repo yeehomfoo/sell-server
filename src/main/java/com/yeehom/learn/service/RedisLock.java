5f9065d76296924b51acbae061ca31f2f52e7b91
Yeehom Foo <yeehom147@gmail.com>
2018-11-24 23:59:42 +0800

    Refactor: add redis lock

---
+++ src/main/java/com/yeehom/learn/service/RedisLock.java
@@ -0,0 +1,62 @@
+package com.yeehom.learn.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author yFoo
+ * @date 24/11/2018
+ */
+
+@Component
+@Slf4j
+public class RedisLock {
+
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
+    /**
+     * 加锁
+     *
+     * @param key
+     * @param value 当前时间+过期时间
+     * @return
+     */
+    public boolean lock(String key, String value) {
+        if (redisTemplate.opsForValue().setIfAbsent(key, value)) {
+            return true;
+        }
+
+        String currentValue = redisTemplate.opsForValue().get(key);
+        // 如果锁过期
+        if (StringUtils.isNotEmpty(currentValue)
+                && Long.valueOf(currentValue) < System.currentTimeMillis()) {
+            // 获取上一个锁的时间
+            String oldValue = redisTemplate.opsForValue().getAndSet(key, value);
+            if (StringUtils.isNotEmpty(oldValue) && oldValue.equals(currentValue)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * 解锁
+     * @param key
+     * @param value
+     */
+    public void unlock(String key, String value) {
+        String currentValue = redisTemplate.opsForValue().get(key);
+        try {
+            if (key == currentValue) {
+                redisTemplate.delete(key);
+            }
+        } catch (Exception e) {
+            log.error("【Redis 分布式锁】解锁失败，异常：", e);
+        }
+    }
+}
